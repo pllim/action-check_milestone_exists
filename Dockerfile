231ab62dc6297faed07f4c73c7bc58c98e3e21d2
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2020-11-17 20:34:58 -0500

    Simplify action

--- Dockerfile
+++ Dockerfile
@@ -5,12 +5,12 @@ RUN apt-get update \
     && apt-get install -y \
     build-essential \
     python3-pip \
-    python3.8 \
+    python3.6 \
     git \
     && python3 -m pip install --upgrade pip \
     && python3 -m pip install --upgrade setuptools \
     && python3 -m pip install --upgrade wheel \
-    && python3 -m pip install git+https://github.com/OpenAstronomy/baldrick#egg=baldrick
+    && python3 -m pip install PyGithub
 
 # Copies code file action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
--- check_milestone.py
+++ check_milestone.py
@@ -1,46 +1,22 @@
 import json
 import os
+import sys
 
-from baldrick.github.github_api import PullRequestHandler
-
-MISSING_MESSAGE = 'Maintainers need to set the milestone for this pull request.'  # noqa
-PRESENT_MESSAGE = 'This pull request has a milestone set.'
+from github import Github
 
 event_jsonfile = os.environ['GITHUB_EVENT_PATH']
-reponame = os.environ['GITHUB_REPOSITORY']
 
 with open(event_jsonfile) as fin:
     event = json.load(fin)
 
 pr_num = event['number']
-pr_handler = PullRequestHandler(reponame, pr_num)
+reponame = os.environ['GITHUB_REPOSITORY']
+g = Github(os.environ.get('GITHUB_TOKEN'))
+repo = g.get_repo(reponame)
+pr = repo.get_pull(pr_num)
 
-if pr_handler.milestone:
-    result = {'name': "milestone: present",
-              'title': PRESENT_MESSAGE,
-              'conclusion': 'success',
-              'summary': ''}
-else:
-    result = {'name': "milestone: absent",
-              'title': MISSING_MESSAGE,
-              'conclusion': 'failure',
-              'summary': ''}
+if not pr.milestone:
+    print('Maintainers need to set the milestone for this pull request.')
+    sys.exit(1)
 
-key = 'milestone'
-set_check = False
-
-# only_ours only work for Flask app
-existing_checks = pr_handler.list_checks(only_ours=False)
-
-# Only update if things have changed or not yet exist
-if key in existing_checks:
-    old_conclusion = existing_checks[key]['conclusion']
-    if old_conclusion != result['conclusion']:
-        set_check = True
-else:
-    set_check = True
-
-if set_check:
-    pr_handler.set_check(key, result['title'], name=result['name'],
-                         summary=result['summary'],
-                         conclusion=result['conclusion'])
+print('This pull request has a milestone set.')
