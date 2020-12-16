52c4bd35de8b7ea58668a15af304f72a27a85505
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2020-12-16 17:50:02 -0500

    Use Docker image, bump Python

--- check_milestone.py
+++ check_milestone.py
@@ -4,6 +4,11 @@ import sys
 
 from github import Github
 
+event_name = os.environ['GITHUB_EVENT_NAME']
+if event_name not in ('pull_request_target', 'pull_request'):
+    print(f'No-op for {event_name}')
+    sys.exit(0)
+
 event_jsonfile = os.environ['GITHUB_EVENT_PATH']
 
 with open(event_jsonfile, encoding='utf-8') as fin:
@@ -13,15 +18,9 @@ pr_num = event['number']
 reponame = event['pull_request']['base']['repo']['full_name']
 g = Github(os.environ.get('GITHUB_TOKEN'))
 repo = g.get_repo(reponame)
-try:
-    pr = repo.get_pull(pr_num)
-except Exception:
-    # GitHub Actions all milestoned/demilestoned events as issue event,
-    # so if this is issue and not PR, it is no-op.
-    pass
+pr = repo.get_pull(pr_num)
+if pr.milestone:
+    print('This pull request has a milestone set.')
 else:
-    if pr.milestone:
-        print('This pull request has a milestone set.')
-    else:
-        print('Maintainers need to set the milestone for this pull request.')
-        sys.exit(1)
+    print('Maintainers need to set the milestone for this pull request.')
+    sys.exit(1)
